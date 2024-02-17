dc09ead11675a7d0196bfc97d3744331215b31f9
Kael Tian <Kael_Tian@163.com>
2024-02-17 14:14:01 +0800

    upgrade codes to remote

---
+++ admin-client/src/test/java/com/example/adminclient/AdminClientApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.adminclient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+@SpringBootTest
+class AdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
